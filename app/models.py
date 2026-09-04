4b9457b61883a147ee7a866330d50f2d51dfd718
PaladinoMaluko <anaspoiltudo@gmail.com>
2026-09-04 16:12:13 -0300

    Montando as relações das tabelas principais e das subtabelas

--- app/models.py
+++ app/models.py
@@ -18,20 +18,41 @@ class Eletronico(Base):
     __tablename__ = 'eletronico'
     id = db.Column(db.Integer, primary_key=True)
     # Tipo do produto 
-    id_produto = db.Column(db.Integer, db.ForeignKey('Produto.id'))
+    id_produto = db.Column(db.Integer, db.ForeignKey('produto.id'))
     marca = db.Column(db.String)
     modelo = db.Column(db.String)
     # Definindo relações (importante: uselist=False -> o produto pode )
     produto = orm.relationship("Produto", back_populates="eletronico", uselist=False)
-    
-class EletroDomestico(Base):
-    __tablename__ = 'eletrodomestico'
+    # Definindo relações com os subtipos
+    eletronicodomestico = orm.relationship("EletronicoDomestico", back_populates="eletronico", uselist=False)
+    eletronicoindustrial = orm.relationship("EletronicoIndustrial", back_populates="eletronico", uselist=False)
+    eletronicointeligente = orm.relationship("EletronicoInteligente", back_populates="eletronico", uselist=False)
+
+# Subcategorias de Eletronicos
+# -----------------------------------------
+class EletronicoDomestico(Base):
+    __tablename__ = 'eletronicodomestico'
     id = db.Column(db.Integer, primary_key=True)
-    id_eletronico = db.Column(db.Integer, db.ForeignKey('Eletronico.id'))
+    id_eletronico = db.Column(db.Integer, db.ForeignKey('eletronico.id'), unique=True)
     cor = db.Column(db.String)
     material = db.Column(db.String)
+    eletronico = orm.relationship("Eletronico", back_populates="eletronicodomestico", uselist=False)
 
+class EletronicoIndustrial(Base):
+    __tablename__ = 'eletronicoindustrial'
+    id = db.Column(db.Integer, primary_key=True)
+    id_eletronico = db.Column(db.Integer, db.ForeignKey('eletronico.id'), unique=True)
+    nicho = db.Column(db.String)
+    material = db.Column(db.String)
+    eletronico = orm.relationship("Eletronico", back_populates="eletronicoindustrial", uselist=False)
 
+class EletronicoInteligente(Base):
+    __tablename__ = 'eletronicointeligente'
+    id = db.Column(db.Integer, primary_key=True)
+    id_eletronico = db.Column(db.Integer, db.ForeignKey('eletronico.id'), unique=True)
+    conectividade = db.Column(db.Boolean)
+    eletronico = orm.relationship("Eletronico", back_populates="eletronicointeligente", uselist=False)
+# -----------------------------------------
 
 Base.metadata.create_all(engine)
 Session = orm.sessionmaker(bind=engine)
